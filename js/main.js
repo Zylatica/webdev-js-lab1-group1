28cf9c68f59f549b86cbc182aa355036e9f47623
Katharine Miller <katy.miller10@icloud.com>
2025-11-04 10:55:12 +0000

    End of Lab 7

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const myHeading = document.getElementById('myHeading')
-myHeading.innerText = "Martin"
+myHeading.innerText = "My Demo Website"
 const firstLink = document.querySelector("nav ul li a")
 firstLink.setAttribute("href","https://www.google.co.uk")
 const colourButtons  = document.querySelectorAll(".colPicker")
@@ -30,3 +30,35 @@ const resetBtn = document.querySelector(".reset")
 resetBtn.addEventListener("click", function(){
     document.body.removeAttribute("class")
 })
+
+// js/main.js
+let imageAr = [
+    'images/view1.jpg',
+    'images/view2.jpg',
+    'images/view3.jpg',
+    'images/view4.jpg',
+    'images/view5.jpg',
+    'images/view6.jpg'
+  ];
+  
+  let currentIndex = 0;
+  
+  function chgImage() {
+    currentIndex++;
+    if (currentIndex >= imageAr.length) {
+      currentIndex = 0;
+    }
+    document.getElementById('myImages').setAttribute('src', imageAr[currentIndex]);
+  }
+
+  let intervalId = setInterval(chgImage, 2000);
+
+  // Stop the interval after 10 seconds
+  setTimeout(function() {
+    clearInterval(intervalId);
+    console.info('Image rotation stopped');
+  }, 10000);
+
+  document.getElementById('myImages').addEventListener('click', chgImage);
+
+  console.dir(window)
